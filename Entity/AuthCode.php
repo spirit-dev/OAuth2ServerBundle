a24dc6b7096e1c71d5840012650784016ef7b98f
Jean Bordat <bordat.jean@gmail.com>
2015-03-06 19:47:13 +0100

    up
    Signed-off-by: Jean Bordat <bordat.jean@gmail.com>

--- Entity/AuthCode.php
+++ Entity/AuthCode.php
@@ -5,12 +5,27 @@ namespace SpiritDev\Bundle\OAuth2ServerBundle\Entity;
 use FOS\OAuthServerBundle\Entity\AuthCode as BaseAuthCode;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Table(name="spiritdev_oauth2_authcode")
+ * @ORM\Entity
+ */
 class AuthCode extends BaseAuthCode
 {
-
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
     protected $client;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     */
     protected $user;
 }
--- Entity/RefreshToken.php
+++ Entity/RefreshToken.php
@@ -5,12 +5,27 @@ namespace SpiritDev\Bundle\OAuth2ServerBundle\Entity;
 use FOS\OAuthServerBundle\Entity\RefreshToken as BaseRefreshToken;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Table(name="spiritdev_oauth2_refreshtoken")
+ * @ORM\Entity
+ */
 class RefreshToken extends BaseRefreshToken
 {
-
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
     protected $client;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     */
     protected $user;
 }
